d06320a7a48a568983c5774e098941922604c523
jianzhnie <jianzhnie@126.com>
2024-03-07 10:11:59 +0800

    update rltoolkit exmaples

--- examples/discrete/discrete_dqn.py
+++ examples/discrete/discrete_dqn.py
@@ -14,13 +14,13 @@ from rltoolkit.policy import DQNPolicy
 from rltoolkit.trainer import OffpolicyTrainer
 from rltoolkit.utils import TensorboardLogger
 from tianshou.data import VectorReplayBuffer
-from tianshou.env import DummyVectorEnv
+from tianshou.env import SubprocVectorEnv
 from tianshou.utils.net.common import Net
 
 
 def get_args() -> Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--task', type=str, default='CartPole-v1')
+    parser.add_argument('--env-id', type=str, default='CartPole-v1')
     parser.add_argument('--algo-name', type=str, default='dqn')
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--eps-test', type=float, default=0.005)
@@ -31,10 +31,11 @@ def get_args() -> Namespace:
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--n-step', type=int, default=3)
     parser.add_argument('--target-update-freq', type=int, default=500)
-    parser.add_argument('--epoch', type=int, default=20)
+    parser.add_argument('--target-update-tau', type=float, default=1.0)
+    parser.add_argument('--epoch', type=int, default=30)
     parser.add_argument('--step-per-epoch', type=int, default=10000)
-    parser.add_argument('--step-per-collect', type=int, default=10)
-    parser.add_argument('--update-per-step', type=float, default=0.1)
+    parser.add_argument('--step-per-collect', type=int, default=500)
+    parser.add_argument('--update-per-step', type=float, default=0.01)
     parser.add_argument('--batch-size', type=int, default=64)
     parser.add_argument('--train-num', type=int, default=10)
     parser.add_argument('--test-num', type=int, default=100)
@@ -64,30 +65,28 @@ def get_args() -> Namespace:
 def main() -> None:
     args = get_args()
     # You can also try SubprocVectorEnv, which will use parallelization
-    train_envs = DummyVectorEnv(
-        [lambda: gym.make(args.task) for _ in range(args.train_num)])
-    test_envs = DummyVectorEnv(
-        [lambda: gym.make(args.task) for _ in range(args.test_num)])
+    train_envs = SubprocVectorEnv(
+        [lambda: gym.make(args.env_id) for _ in range(args.train_num)])
+    test_envs = SubprocVectorEnv(
+        [lambda: gym.make(args.env_id) for _ in range(args.test_num)])
 
     # Note: You can easily define other networks.
-    # See https://tianshou.readthedocs.io/en/master/01_tutorials/00_dqn.html#build-the-network
-    env = gym.make(args.task, render_mode='human')
+    env = gym.make(args.env_id, render_mode='human')
     state_shape = env.observation_space.shape or env.observation_space.n
     action_shape = env.action_space.shape or env.action_space.n
     args.total_steps = args.epoch * args.step_per_epoch
     print('Observations shape:', state_shape)
     print('Actions shape:', action_shape)
+    # seed
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-
     # define model
     net = Net(
         state_shape=state_shape,
         action_shape=action_shape,
-        hidden_sizes=[128, 128, 128],
+        hidden_sizes=[128, 64],
         device=args.device,
     ).to(args.device)
-
     optim = torch.optim.Adam(net.parameters(), lr=args.lr)
 
     # define policy
@@ -98,6 +97,7 @@ def main() -> None:
         action_space=env.action_space,
         estimation_step=args.n_step,
         target_update_freq=args.target_update_freq,
+        target_update_tau=args.target_update_tau,
     )
 
     # load a previous policy
@@ -121,13 +121,11 @@ def main() -> None:
         exploration_noise=True,
     )  # because DQN uses epsilon-greedy method
 
-    log_name = os.path.join(args.task, args.algo_name)
+    log_name = os.path.join(args.env_id, args.algo_name)
     log_path = os.path.join(args.logdir, log_name)
 
-    logger = TensorboardLogger(
-        SummaryWriter(log_path))  # TensorBoard is supported!
-
-    # For other loggers, see https://tianshou.readthedocs.io/en/master/tutorials/logger.html
+    # TensorBoard is supported!
+    logger = TensorboardLogger(SummaryWriter(log_path))
 
     def save_best_fn(policy):
         torch.save(policy.state_dict(), os.path.join(log_path, 'policy.pth'))
@@ -138,7 +136,7 @@ def main() -> None:
         torch.save({'model': policy.state_dict()}, ckpt_path)
         return ckpt_path
 
-    def train_fn(epoch, env_step):
+    def train_fn(epoch, env_step) -> None:
         # nature DQN setting, linear decay in the first 1M steps
         if env_step <= args.total_steps:
             eps = args.eps_train - env_step / args.total_steps * (
@@ -149,7 +147,7 @@ def main() -> None:
         if env_step % 100 == 0:
             logger.write('train/env_step', env_step, {'train/eps': eps})
 
-    def test_fn(epoch, env_step):
+    def test_fn(epoch, env_step) -> None:
         policy.set_eps(args.eps_test)
 
     def stop_fn(mean_rewards: float) -> bool:
@@ -161,7 +159,7 @@ def main() -> None:
         return False
 
     # watch agent's performance
-    def watch():
+    def watch() -> None:
         print('Setup test envs ...')
         policy.eval()
         policy.set_eps(args.eps_test)
@@ -169,8 +167,10 @@ def main() -> None:
         test_collector.reset()
         result = test_collector.collect(n_episode=args.test_num,
                                         render=args.render)
-        rew = result['rews'].mean()
-        print(f"Mean reward (over {result['n/ep']} episodes): {rew}")
+        reward_mean = result['episode_reward'].mean()
+        length_mean = result['episode_length'].mean()
+        print(f"Mean reward (over {result['num_episode']} episodes): "
+              f'{reward_mean}, mean length: {length_mean}')
 
     if args.watch:
         watch()
---
+++ examples/discrete/train_dqn.sh
@@ -0,0 +1,5 @@
+python discrete_dqn.py \
+    --algo dqn \
+    --resume-path ./log/CartPole-v1/dqn/policy.pth \
+    --render 0.2 \
+    --watch
