47c4a9053e33e21e106798fb686e1ab99b4b45f2
jianzhnie <jianzhnie@126.com>
2024-03-14 15:40:37 +0800

    update rltoolkit

--- rltoolkit/policy/__init__.py
+++ rltoolkit/policy/__init__.py
@@ -1,8 +1,17 @@
 from .base_policy import BasePolicy
+from .modelfree.c51 import C51Policy
 from .modelfree.dqn import DQNPolicy
+from .modelfree.pg import PGPolicy
+from .modelfree.rainbow import RainbowPolicy
 from .multiagent.mapolicy import MultiAgentPolicyManager
 from .random_policy import RandomPolicy
 
 __all__ = [
-    'BasePolicy', 'DQNPolicy', 'RandomPolicy', 'MultiAgentPolicyManager'
+    'BasePolicy',
+    'DQNPolicy',
+    'RandomPolicy',
+    'MultiAgentPolicyManager',
+    'PGPolicy',  # Policy Gradient]
+    'C51Policy',  # Categorical 51
+    'RainbowPolicy',  # Rainbow
 ]
