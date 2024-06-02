2c9cceac28f713cce5e5843415854ce87e112603
jianzhnie <jianzhnie@126.com>
2024-06-02 18:01:30 +0800

    Update __init__.py

--- rltoolkit/agents/__init__.py
+++ rltoolkit/agents/__init__.py
@@ -1,4 +1,4 @@
 from .base_agent import BaseAgent
-from .dqn import DQNAgent, DQNConfig
+from .dqn import DQNAgent
 
-__all__ = ['DQNAgent', 'BaseAgent', 'DQNConfig']
+__all__ = ['DQNAgent', 'BaseAgent']
